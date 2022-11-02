e760d0243a61bc66a3bf961e09a0f1005eb8ad3e
Johan Klasén <johkl473@student.liu.se>
2022-11-02 09:59:25 +0100

    Initial atmel studio sensor project

---
+++ sensor-module/SensorModule/SensorModule/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * SensorModule.cpp
+ *
+ * Created: 2022-11-02 08:26:16
+ * Author : johkl473
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
