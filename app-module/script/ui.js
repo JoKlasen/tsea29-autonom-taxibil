dd4a63f6fb14503dac783edd030a6ffcb68ecca9
Linus Thorsell <thorsell.linus@gmail.com>
2022-12-05 14:14:53 +0100

    changed commands to shorthand and added mission setup ui and command

--- app-module/script/ui.js
+++ app-module/script/ui.js
@@ -6,19 +6,28 @@ function switchMode() {
 
     if (mode == 1) {
         document.getElementById("control-mode").innerHTML = "Manual"
-        sendMessage("switchmode:mode=1:")
+        sendMessage("sm:m=1:")
     }
     else {
         document.getElementById("control-mode").innerHTML = "Auto"
-        sendMessage("switchmode:mode=0:")
+        sendMessage("sm:m=0:")
     }
 }
 
 function sendPID() {
-    let pidvalues = "sendpid" +
+    let pidvalues = "sp" +
         ":p=" + document.getElementById("control-pid-p").value +
         ":i=" + document.getElementById("control-pid-i").value +
-        ":d=" + document.getElementById("control-pid-d").value
+        ":d=" + document.getElementById("control-pid-d").value + ':'
 
     sendMessage(pidvalues)
+}
+
+function sendMission() {
+    let mission = "mi" +
+        ":s=" + document.getElementById("mi-s").value +
+        ":p=" + document.getElementById("mi-p").value +
+        ":e=" + document.getElementById("mi-e").value + ':'
+
+    sendMessage(mission)
 }
